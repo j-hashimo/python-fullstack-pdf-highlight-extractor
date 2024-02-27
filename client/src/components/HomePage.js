422dda18d953765cc3bd75f3e3cea6deb58754ae
Josh <hashimotojosh@gmail.com>
2024-02-26 19:11:16 -0800

    added button for user to directly see previous pdfs files and highlights. next work on errors, frontend appearance, and non-user homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,58 +1,80 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, CardHeader, CardBody, CardFooter, Box, Text, Heading, Stack, StackDivider } from '@chakra-ui/react'
-
+import { Card, CardHeader, CardBody, Box, Text, Heading, Stack, StackDivider } from '@chakra-ui/react'
 
 const HomePage = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-900">
-            <Card className="ml-5 mr-5">
-            <CardHeader>
-                <Heading size='md'>PDF Highlight Extractor</Heading>
-            </CardHeader>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900">
+            <Card className="mb-5 ml-5 mr-5">
+                <CardHeader>
+                    <Heading size='md'>PDF Highlight Extractor</Heading>
+                </CardHeader>
 
-            <CardBody>
-                <Stack divider={<StackDivider />} spacing='4'>
-                <Box>
-                    <Heading size='xs' textTransform='uppercase'>
-                    Upload Highlights
-                    </Heading>
-                    <Text pt='2' fontSize='sm'>
-                    Click the button below to upload your pdf file for highlight extraction.
-                    </Text>
-                </Box>
-                <Box>
-                    <div className="text-center">
-                    <Link to="/upload" className="mt-8 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Get Started</Link>
-                    </div>
-                </Box>
-                </Stack>
-            </CardBody>
+                <CardBody>
+                    <Stack divider={<StackDivider />} spacing='4'>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                Upload Highlights
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                Click the button below to upload your pdf file for highlight extraction.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <div className="text-center">
+                                <Link to="/upload" className="mt-8 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Get Started</Link>
+                            </div>
+                        </Box>
+                    </Stack>
+                </CardBody>
             </Card>
             
-            <Card className="ml-5 mr-5">
-            <CardHeader>
-                <Heading size='md'>PDF Image Extractor</Heading>
-            </CardHeader>
+            <Card className="mb-5 ml-5 mr-5">
+                <CardHeader>
+                    <Heading size='md'>PDF Image Extractor</Heading>
+                </CardHeader>
 
-            <CardBody>
-                <Stack divider={<StackDivider />} spacing='4'>
-                <Box>
-                    <Heading size='xs' textTransform='uppercase'>
-                    Upload Images
-                    </Heading>
-                    <Text pt='2' fontSize='sm'>
-                    Click the button below to upload your pdf file for image extraction.
-                    </Text>
-                </Box>
-                <Box>
-                    <div className="text-center">
-                        
-                        <Link to="/upload_for_images" className="mt-8 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Upload images</Link>
-                    </div>
-                </Box>
-                </Stack>
-            </CardBody>
+                <CardBody>
+                    <Stack divider={<StackDivider />} spacing='4'>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                Upload Images
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                Click the button below to upload your pdf file for image extraction.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <div className="text-center">
+                                <Link to="/upload_for_images" className="mt-8 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Upload Images</Link>
+                            </div>
+                        </Box>
+                    </Stack>
+                </CardBody>
+            </Card>
+
+            <Card className="ml-5 mr-5">
+                <CardHeader>
+                    <Heading size='md'>View PDFs</Heading>
+                </CardHeader>
+
+                <CardBody>
+                    <Stack divider={<StackDivider />} spacing='4'>
+                        <Box>
+                            <Heading size='xs' textTransform='uppercase'>
+                                View and Download PDFs
+                            </Heading>
+                            <Text pt='2' fontSize='sm'>
+                                Access your PDF files and highlights. No upload required.
+                            </Text>
+                        </Box>
+                        <Box>
+                            <div className="text-center">
+                                <Link to="/pdfview" className="mt-8 px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded">View PDFs</Link>
+                            </div>
+                        </Box>
+                    </Stack>
+                </CardBody>
             </Card>
         </div>
     )
